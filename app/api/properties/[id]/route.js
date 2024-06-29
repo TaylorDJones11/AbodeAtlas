cb028b595a87b0b0a73997d1ddcb1b2ce3d4e3e0
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-06-29 13:37:19 -0500

    fetching a single property

---
+++ app/api/properties/[id]/route.js
@@ -0,0 +1,22 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+
+// GET /api/properties/:d
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectDB();
+    console.log('Database connected');
+
+    const property = await Property.findById(params.id);
+
+    if (!property) {
+      return Response.json({ message: 'Property Not Found' }, { status: 404 });
+    }
+    console.log(property);
+
+    return Response.json(property);
+  } catch (error) {
+    return Response.json({ message: "It's an error" }, { status: 500 });
+  }
+};
